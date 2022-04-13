81a21f8eda9426bf3565e47ddeeeefeca6c901db
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-04-13 22:09:55 +0700

    Fixes from shuyshuys >..<

--- cBarang.java
+++ cBarang.java
@@ -1,28 +1,32 @@
-package finalproject;
+package git.ProjectUTS;
+
 public class cBarang {
     private String nama;
     private int harga;
 
-    cBarang (String nama, int harga){
-     this.nama = nama; this.harga = harga;
-     System.out.println("Menu  "+nama+"  Dibuat");
+    cBarang(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+        System.out.println("Menu  " + nama + "  Dibuat");
     }
 
-    public void setNama(String nama){
-        this.nama= nama;
+    public void setNama(String nama) {
+        this.nama = nama;
     }
 
-    public void setHarga (int harga){
+    public void setHarga(int harga) {
         this.harga = harga;
     }
-    public String getNama(){
+
+    public String getNama() {
         return nama;
     }
 
-    public int getHarga(){
+    public int getHarga() {
         return harga;
     }
-    public String ToString(){
-        return "Menu : "+nama+" ("+harga+")";
+
+    public String ToString() {
+        return "Menu : " + nama + "\t(" + harga + ")";
     }
 }
--- cMakanan.java
+++ cMakanan.java
@@ -1,11 +1,7 @@
-package finalproject;
-public class cMakanan extends cBarang{
-cMakanan(String nama, int harga){
-    super(nama,harga);
-}
-        
-        
-        
-}
+package git.ProjectUTS;
 
-     
+public class cMakanan extends cBarang {
+    cMakanan(String nama, int harga) {
+        super(nama, harga);
+    }
+}
